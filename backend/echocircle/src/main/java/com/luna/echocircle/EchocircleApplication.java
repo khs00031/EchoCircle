d3c64bfd3ff5432f98dab29cc25922e847a5f25f
khs00031 <83671232+khs00031@users.noreply.github.com>
2024-07-04 19:50:48 +0900

    초기파일 업로드

---
+++ backend/echocircle/src/main/java/com/luna/echocircle/EchocircleApplication.java
@@ -0,0 +1,13 @@
+package com.luna.echocircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchocircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchocircleApplication.class, args);
+	}
+
+}
---
+++ backend/echocircle/src/test/java/com/luna/echocircle/EchocircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luna.echocircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchocircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
