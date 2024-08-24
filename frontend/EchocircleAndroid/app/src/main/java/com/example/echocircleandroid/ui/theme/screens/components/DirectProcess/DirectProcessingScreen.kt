cdb6c01eebef15b0d50828636c7cf81b8335b333
jini11 <lje8213@gmail.com>
2024-08-24 23:11:22 +0900

    update: 서비스 메인 페이지 UI 수정

--- frontend/EchocircleAndroid/app/src/main/java/com/example/echocircleandroid/ui/theme/screens/components/DirectProcess/DirectProcessingScreen.kt
+++ frontend/EchocircleAndroid/app/src/main/java/com/example/echocircleandroid/ui/theme/screens/components/DirectProcess/DirectProcessingScreen.kt
@@ -1,6 +1,8 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -25,35 +27,46 @@ fun DirectProcessingPreview(){
 }
 @Composable
 fun UserGuideScreen(navController: NavController){
-    Column(
+    Box(
         modifier = Modifier
+            .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "수거하실 폐가전의 바코드 번호를 찍어주세요.",
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
             modifier = Modifier
                 .padding(16.dp),
-            fontSize = 16.sp
-        )
-        Image(
-            painter = painterResource(id = R.drawable.deviceuserguide),
-            contentDescription = "사용자 촬영 설명",
-            modifier = Modifier
-                .padding(16.dp)
-        )
-        Button(
-            onClick = {navController.navigate("start_camera")},
-            modifier = Modifier
-                .padding(16.dp)
-        ){
-            Text(text = "촬영하기")
-        }
-        Text(
-            text = "바코드가 없으신가요?",
-            style = TextStyle(color = Color.Blue, textDecoration = TextDecoration.Underline, fontSize = 10.sp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "수거하실 폐가전의 바코드 번호를 찍어주세요.",
+                modifier = Modifier
+                    .padding(16.dp),
+                fontSize = 16.sp
+            )
+            Image(
+                painter = painterResource(id = R.drawable.deviceuserguide),
+                contentDescription = "사용자 촬영 설명",
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+            Button(
+                onClick = { navController.navigate("start_camera") },
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(text = "촬영하기")
+            }
+            Text(
+                text = "바코드가 없으신가요?",
+                style = TextStyle(
+                    color = Color.Blue,
+                    textDecoration = TextDecoration.Underline,
+                    fontSize = 10.sp
+                ),
 
-        )
+                )
+        }
     }
 }
--- frontend/EchocircleAndroid/app/src/main/java/com/example/echocircleandroid/ui/theme/screens/components/LoginScreen.kt
+++ frontend/EchocircleAndroid/app/src/main/java/com/example/echocircleandroid/ui/theme/screens/components/LoginScreen.kt
@@ -1,9 +1,11 @@
 package com.example.echocircleandroid.ui.theme.screens
 
 import BottomNavItem
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,10 +19,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.echocircleandroid.R
 
@@ -30,6 +30,7 @@ fun LoginScreen(navController : NavHostController){
     Surface(
         modifier = Modifier
             .fillMaxSize(),
+        color = colorResource(id = R.color.green)
     ) {
         Column(
             modifier = Modifier
@@ -38,22 +39,34 @@ fun LoginScreen(navController : NavHostController){
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = stringResource(id = R.string.app_name),
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold,
-                color = colorResource(id = R.color.black) // 보라색 텍스트
+            Image(
+                painter = painterResource(id = R.drawable.sub_logo),
+                contentDescription = "서브 로고",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.4f)
+                    .padding(horizontal = 30.dp)
             )
+//            Text(
+//                text = stringResource(id = R.string.app_name),
+//                fontSize = 32.sp,
+//                fontWeight = FontWeight.Bold,
+//                color = colorResource(id = R.color.black) // 보라색 텍스트
+//            )
 
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(40.dp))
 
             Button(
                 onClick = { /*TODO*/ },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .height(50.dp)
+                    .padding(horizontal = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                )
             ) {
-                Text(text = "Google Login")
+                Text(text = "구글 로그인", color = Color.Black)
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -62,10 +75,13 @@ fun LoginScreen(navController : NavHostController){
                 onClick = { /*TODO*/ },
                 modifier = Modifier
                     .fillMaxWidth()
+                    .height(50.dp)
                     .padding(horizontal = 16.dp),
-                colors = ButtonDefaults.buttonColors(Color(0xFF3B5998)) // 카카오 색상
+//                colors = ButtonDefaults.buttonColors(Color(0xFF3B5998)) // 카카오 색상
+//                colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.yellow))
             ) {
-                Text(text = "KaKao Login", color = Color.White)
+                Text(text = "카카오 로그인", color = Color.Black)
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -74,9 +90,11 @@ fun LoginScreen(navController : NavHostController){
                 onClick = { navController.navigate(BottomNavItem.DirectProcessing.screen_route) },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
+                    .height(50.dp)
+                    .padding(horizontal = 16.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.dark_green))
             ) {
-                Text(text = "비회원 로그인")
+                Text(text = "비회원 로그인", color = Color.Black)
             }
         }
     }
